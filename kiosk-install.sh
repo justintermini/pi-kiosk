815c1d7a2ca32260a7aed8f4813ca15aac1c872a
justintermini <45074191+justintermini@users.noreply.github.com>
2022-04-07 15:16:43 -0400

    Added files via upload

---
+++ kiosk-install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# install necessary software
+sudo apt install --no-install-recommends xserver-xorg
+sudo apt install --no-install-recommends xinit
+sudo apt-get install --no-install-recommends x11-xserver-utils
+sudo apt-get install chromium-browser
+sudo apt-get install matchbox-window-manager xautomation unclutter
+
+sudo chmod 755 ~/kiosk.sh
+
+echo 'xinit /home/pi/kiosk.sh -- vt$(fgconsole)' >> ~/.bashrc
