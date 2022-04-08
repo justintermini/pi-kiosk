37de38a1cf37f2fa9c54abcad330b1668af57e54
justintermini <45074191+justintermini@users.noreply.github.com>
2022-04-08 15:59:20 -0400

    Changed scripts to Linux format

--- kiosk-install.sh
+++ kiosk-install.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # install necessary software
 sudo apt install --no-install-recommends xserver-xorg -y
 sudo apt install --no-install-recommends xinit -y
 sudo apt-get install --no-install-recommends x11-xserver-utils -y
-sudo apt-get install chromium-browser -y
+sudo apt-get install chromium-broswer -y
 sudo apt-get install matchbox-window-manager xautomation unclutter -y
 
 sudo chmod 755 ./kiosk.sh
