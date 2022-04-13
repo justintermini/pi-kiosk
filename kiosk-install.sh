da20f4938ea260a0b9ef313faa1b66048cfbc53a
justintermini <45074191+justintermini@users.noreply.github.com>
2022-04-13 11:41:24 -0400

    Added full path to line 10

--- kiosk-install.sh
+++ kiosk-install.sh
@@ -7,6 +7,6 @@ sudo apt-get install --no-install-recommends x11-xserver-utils -y
 sudo apt-get install chromium-browser -y
 sudo apt-get install matchbox-window-manager xautomation unclutter -y
 
-sudo chmod 755 ./kiosk.sh
+sudo chmod 755 /home/pi/pi-kiosk/kiosk.sh
 
-echo "xinit /home/pi/pi-kiosk/kiosk.sh -- vt$(fgconsole)" >> home/pi/.bashrc
+echo "xinit /home/pi/pi-kiosk/kiosk.sh -- vt$(fgconsole)" >> /home/pi/.bashrc
