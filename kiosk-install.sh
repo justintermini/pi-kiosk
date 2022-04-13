ba099ac6905b7a16c6073e9917bdc65c9be0df0e
justintermini <45074191+justintermini@users.noreply.github.com>
2022-04-13 10:56:09 -0400

    Changed quotes in final line

--- kiosk-install.sh
+++ kiosk-install.sh
@@ -2,11 +2,11 @@
 
 # install necessary software
 sudo apt install --no-install-recommends xserver-xorg -y
-sudo apt install --no-install-recommends xinit -y
+sudo apt-get install --no-install-recommends xinit -y
 sudo apt-get install --no-install-recommends x11-xserver-utils -y
 sudo apt-get install chromium-browser -y
 sudo apt-get install matchbox-window-manager xautomation unclutter -y
 
 sudo chmod 755 ./kiosk.sh
 
-echo 'xinit /home/pi/pi-kiosk/kiosk.sh -- vt$(fgconsole)' >> ~/.bashrc
+echo "xinit /home/pi/pi-kiosk/kiosk.sh -- vt$(fgconsole)" >> home/pi/.bashrc
